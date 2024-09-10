0d9695d50e2563a7bf22b72a582233e960347d51
Anil <anilsolanki1834@gmail.com>
2024-09-10 23:13:56 +0530

    Update File for snowflake Database

--- database_management_queries_snowflake.sql
+++ database_management_queries_snowflake.sql
@@ -7,3 +7,8 @@
 -- [1] Create a New Database:
 
 CREATE DATABASE GALAXY;
+
+-- [2] Create a Database with a Comment and Replace if exists:
+
+CREATE OR REPLACE DATABASE GALAXY 
+COMMENT = 'THIS IS GALAXY DATABASE THAT STORE GALAXY INFORMATION';
