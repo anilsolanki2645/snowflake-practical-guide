d1994d28ea1e551075ba5fa9940fe834ae31ddc8
Anil <anilsolanki1834@gmail.com>
2024-09-10 23:13:07 +0530

    Add new File for snowflake Database

---
+++ database_management_queries_snowflake.sql
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------------------
+                                    -- DATABASE MANAGEMENT QUERIES
+-----------------------------------------------------------------------------------------------------
+
+-- Here's a comprehensive list of unique queries related to databases in Snowflake. These queries cover the creation, modification, management, and inspection of databases.
+
+-- [1] Create a New Database:
+
+CREATE DATABASE GALAXY;
