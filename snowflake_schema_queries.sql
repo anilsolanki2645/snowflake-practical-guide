4647423f68db95f5a7dcdc6cc771223d6b873e42
Anil <anilsolanki1834@gmail.com>
2024-09-15 22:21:10 +0530

    Update File for snowflake Schemas

--- snowflake_schema_queries.sql
+++ snowflake_schema_queries.sql
@@ -3,3 +3,8 @@
 -----------------------------------------------------------------------------------------------------
 
 -- Here are unique queries for Schema Types and schemas in Snowflake:
+
+-- [1] Create a Schema:
+
+USE DATABASE GALAXY; -- Use Database Before Create a Schema
+CREATE SCHEMA GALAXY_SCHEMA;
