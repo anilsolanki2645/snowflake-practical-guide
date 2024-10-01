2282abbc533ec3fbebabdb56927354dc55daf397
Anil <anilsolanki1834@gmail.com>
2024-10-01 23:25:44 +0530

    Create a new file Snowflake Tables Queries

---
+++ snowflake_Tables_queries.sql
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------------------
+                                    -- TABLES RELATED QUERIES
+-----------------------------------------------------------------------------------------------------
+
+-- create database and schema to create a table in that
+
+CREATE OR REPLACE DATABASE EMP;
+
+CREATE OR REPLACE SCHEMA EMP_SCHEMA;
